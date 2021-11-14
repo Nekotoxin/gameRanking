1578232df88ac4ac7634e41566ba717a390f7b9a
Nekotoxin <nekotoxin@outlook.com>
2021-11-14 22:42:45 +0800

    $

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
+#url_prefix：设置蓝图的URL前缀
 AuthBP = Blueprint('auth', __name__, url_prefix='/auth',template_folder='templates')
-GameBP = Blueprint('blog', __name__,template_folder='templates')
-HomeBp=Blueprint('home',__name__,template_folder='templates')
+GameBP = Blueprint('blog', __name__,url_prefix='/game',template_folder='templates')
+HomeBp=Blueprint('home',__name__,url_prefix='/user',template_folder='templates')
 MainPageBP=Blueprint('mainpage',__name__,template_folder='templates')
