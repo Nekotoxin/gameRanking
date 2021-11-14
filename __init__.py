f76734675c5546102da24ff8c6833b051048d93e
Nekotoxin <nekotoxin@outlook.com>
2021-11-14 22:39:45 +0800

    蓝图说明

--- __init__.py
+++ __init__.py
@@ -11,16 +11,17 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello, World!'
-
+    #用户注册 登录界面
     from apps import AuthBP
     app.register_blueprint(AuthBP)
-
+    #单个游戏的页面，会有用户的评论，感想和游戏的详细信息
     from apps import GameBP
     app.register_blueprint(GameBP)
 
+    #HomeBP:用户自己主页(功能有修改密码，用户名，放置用户发的感想等)的蓝图
     from apps import HomeBP
     app.register_blueprint(HomeBP)
-
+    #网站主页(展示游戏排行榜)的蓝图
     from apps import MainPageBP
     app.register_blueprint(MainPageBP)
 
