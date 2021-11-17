3fb5509ed3cc627ac752f9e0a8a64dc0009a6f35
hughdazz <hughdazz280@163.com>
2021-11-17 14:48:44 +0800

    '简单的定义了表类，还待修改'

--- db.py
+++ db.py
@@ -1,39 +1,34 @@
 #文件名:db.py
 #功能：数据库操作
 #功能函数包括：初始化数据库，插入数据，查询数据，更新数据，删除数据，关闭数据库,按照评分排序
-import click
-from flask import current_app, g,session
-from flask.cli import with_appcontext
-import pymysql
-#######################
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-HOSTNAME='localhost'
-USERNAME='root'
-PASSWORD='000000'
-DATABASENAME='gamerank'
-
-db=pymysql.connect(host=HOSTNAME,user=USERNAME,password=PASSWORD,db=DATABASENAME,charset='utf8')
-
+db=SQLAlchemy()
 def init_app(app):
-    #app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+    db = SQLAlchemy(app)
 
-def init_db():
-    return 0
 
-def register_user(username,password):
-    return 0
+class game_info(db.Model):
+    game_id=db.Column(db.Integer, primary_key=True)
+    game_title=db.Column(db.String(80), unique=True, nullable=False)
+    game_score=db.Column(db.Float)
+    game_type=db.Column(db.String(30))
+    game_intro=db.Column(db.String(400))
+    game_graph_path=db.Column(db.String(100))#相对目录下文件名称，以^分割
+    game_update_time=db.Colum(db.DateTime)
+    game_collect_num=db.Column(db.Integer)
+    game_comments_num=db.Column(db.Integer)
+class comments(db.Model):
+    comments_id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user_info.user_id'))
+    comment_time=db.Colum(db.DateTime)
+    conment_contents=db.Column(db.String(400))
 
-def login_user(username,password):
-    return 0
-
-# def close_db():
-#     return 0
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
+class userinfo(db.Model):
+    user_id=db.Column(db.Integer, primary_key=True)
+    user_name=db.Column(db.String(80), unique=True, nullable=False)
+    user_regis_time=db.Colum(db.DateTime)
+    user_self_intro=db.Column(db.String(400))
+    
 
